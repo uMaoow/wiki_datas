b9c7e202fa61091ea67f52244d0b99ba7832e48f
uMaoow <cmaoow@gmail.com>
2024-03-03 19:38:38 +0100

    function to check if an article is related to a theme

--- parse_contribs.py
+++ parse_contribs.py
@@ -4,6 +4,15 @@ import re
 import sys
 
 contributions = {}
+contributions_url = {}
+trans_theme = [
+
+        "transidentité",
+        "transgenre",
+        "non binaire",
+        "non-binaire",
+        "transition de genre"
+        ]
 
 def get_contribution_list(user, limit):
     html_list = ""
@@ -36,15 +45,29 @@ def get_contributions(html_list, limit):
             line = buf.readline()
             res = line.rfind(target)
         if line != "":
-            title = re.findall('title=".*"', line)[0][7:-1]
+            title = re.findall('title=".*?"', line)[0][7:-1]
+            url = re.findall('href=".*?"', line)[0][6:-1]
             if title in contributions:
                 contributions[title] += 1
             else:
                 contributions[title] = 1
+                contributions_url[title] = url
     return(contributions)
 
+def is_theme_related(page_url, theme):
+    url = "https://fr.wikipedia.org" + page_url
+    html = urlopen(url)
+    page = html.read().decode().lower()
+    for target in theme:
+        if page.rfind(target) != -1:
+            return True
+    return False
 
 if __name__ == "__main__":
+    count = 0
+    theme_count = 0
+    edit_count = 0
+    edit_theme_count = 0
     if len(sys.argv) < 3:
         print("usage: ./parse_contribs.py user limit [min]")
     get_contribution_list(sys.argv[1],sys.argv[2])
@@ -53,6 +76,15 @@ if __name__ == "__main__":
         if len(sys.argv) > 3:
             if item[1] < int(sys.argv[3]):
                 break
+        print("\n")
         print(item)
+        count += 1
+        edit_count += item[1]
+        if is_theme_related(contributions_url[item[0]], trans_theme) :
+            theme_count += 1
+            edit_theme_count += item[1]
+            print ("trans related")
 
 
+    print(str(theme_count) + " out of " + str(count))
+    print(str(edit_theme_count) + " out of " + str(edit_count))
