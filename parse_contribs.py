77bcf03165eeb14d279c9aaf98159f21eca36132
uMaoow <cmaoow@gmail.com>
2024-03-03 13:24:23 +0100

    avoid infinite loop, display usage, accept a minimum edition filter

--- parse_contribs.py
+++ parse_contribs.py
@@ -11,13 +11,15 @@ def get_contribution_list(user, limit):
     url = "https://fr.wikipedia.org/w/index.php?title=Sp%C3%A9cial:Contributions/" + user + "&target=" + user + "&offset=&limit=" + str(limit)
     html = urlopen(url)
     res = -1
-    while res == -1:
+    line = "\n"
+    while res == -1 and line != "":
         line = html.readline().decode()
         res = line.rfind(target)
 
     target = "</section>"
     res = -1
-    while res == -1:
+    line = "\n"
+    while res == -1 and line != "":
         line = html.readline().decode()
         html_list += line
         res = line.rfind(target)
@@ -43,9 +45,14 @@ def get_contributions(html_list, limit):
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("usage: ./parse_contribs.py user limit [min]")
     get_contribution_list(sys.argv[1],sys.argv[2])
     sorted_list = sorted(contributions.items(), key=lambda item: item[1], reverse=True)
     for item in sorted_list:
+        if len(sys.argv) > 3:
+            if item[1] < int(sys.argv[3]):
+                break
         print(item)
 
 
